9f7eebb377a4dc286dfcf9ec49e15ed1a3adf7b6
Daniel Katulevskiy <katulevskiy@gmail.com>
2024-04-14 16:08:45 -0700

    all signed copies

--- admin/misc/sign.py
+++ admin/misc/sign.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 df = pd.read_csv("../people.csv")
 for name in df["Name"]:
-    print("rules_" + "".join(name.to_lower().split()) + ".md")
+    print("cp rules.pdf rules_" + "".join(name.lower().split()) + ".pdf")
