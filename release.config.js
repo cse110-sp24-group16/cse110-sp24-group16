86dfadcf4c4ebf4d8cc73294a293e4293b08d3a0
Daniel Katulevskiy <katulevskiy@gmail.com>
2024-06-08 03:46:59 -0700

    fix: add binaries to releases automatically

--- release.config.js
+++ release.config.js
@@ -8,12 +8,13 @@ const config = {
       {
         assets: [
           "./out/make/deb/arm64/*.deb",
-          ".out/make/deb/x64/*.deb",
+          "./out/make/deb/x64/*.deb",
           "./out/make/rpm/arm64/*.rpm",
-          "./out/make/rpm/arm64/*.rpm",
-          ".out/make/squirrel.windows/arm64/*.exe*",
-          ".out/make/squirrel.windows/x64/*.exe*",
-          ".out/make/zip/darwin/x64/*.zip",
+          "./out/make/rpm/x64/*.rpm",
+          "./out/make/squirrel.windows/arm64/*.exe*",
+          "./out/make/squirrel.windows/x64/*.exe*",
+          "./out/make/zip/darwin/arm64/*.zip",
+          "./out/make/zip/darwin/x64/*.zip",
         ],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
