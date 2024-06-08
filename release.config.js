9511323d8a560d061223ac1753793def87ff1eb4
Daniel Katulevskiy <katulevskiy@gmail.com>
2024-06-08 02:21:18 -0700

    change over to electron-forge

--- release.config.js
+++ release.config.js
@@ -6,7 +6,15 @@ const config = {
     [
       "@semantic-release/git",
       {
-        assets: ["builds/*.zip"],
+        assets: [
+          "./out/make/deb/arm64/*.deb",
+          ".out/make/deb/x64/*.deb",
+          "./out/make/rpm/arm64/*.rpm",
+          "./out/make/rpm/arm64/*.rpm",
+          ".out/make/squirrel.windows/arm64/*.exe*",
+          ".out/make/squirrel.windows/x64/*.exe*",
+          ".out/make/zip/darwin/x64/*.zip",
+        ],
         message:
           "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
       },
