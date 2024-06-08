3e257354aa07c49efb61e1f616a6f69ed8cc78e0
Daniel Katulevskiy <katulevskiy@gmail.com>
2024-06-08 06:10:24 -0700

    fix: workflow automation edits

--- release.config.js
+++ release.config.js
@@ -48,6 +48,47 @@ const config = {
       },
     ],
     "@semantic-release/github",
+    {
+      assets: [
+        {
+          path: "out/make/deb/arm64/*.deb",
+          name: "deb_arm64-${nextRelease.gitTag}.deb",
+          label: "deb_arm64 (${nextRelease.gitTag}) distribution",
+        },
+        {
+          path: "out/make/deb/x64/*.deb",
+          name: "deb_x64-${nextRelease.gitTag}.deb",
+          label: "deb_x64 (${nextRelease.gitTag}) distribution",
+        },
+        {
+          path: "out/make/rpm/arm64/*.rpm",
+          name: "rpm_arm64-${nextRelease.gitTag}.rpm",
+          label: "rpm_arm64 (${nextRelease.gitTag}) distribution",
+        },
+        {
+          path: "out/make/rpm/x64/*.rpm",
+          name: "rpm_x64-${nextRelease.gitTag}.rpm",
+          label: "rpm_x64 (${nextRelease.gitTag}) distribution",
+        },
+        {
+          path: "out/make/squirrel.windows/x64/*.exe*",
+          name: "win_x64-${nextRelease.gitTag}.exe",
+          label: "win_x64 (${nextRelease.gitTag}) distribution",
+        },
+        {
+          path: "out/make/zip/darwin/arm64/*.zip",
+          name: "mac_arm64-${nextRelease.gitTag}.zip",
+          label: "mac_arm64 (${nextRelease.gitTag}) distribution",
+        },
+        {
+          path: "out/make/zip/darwin/x64/*.zip",
+          name: "mac_x64-${nextRelease.gitTag}.zip",
+          label: "mac_x64 (${nextRelease.gitTag}) distribution",
+        },
+      ],
+      message:
+        "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}",
+    },
   ],
 };
 
