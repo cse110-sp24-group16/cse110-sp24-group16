f3afc822afe6acc2ef8c2a2ddbf87be6cef06c30
Daniel Katulevskiy <katulevskiy@gmail.com>
2024-06-08 07:43:15 -0700

    fix: wait 2 seconds to see the browser

--- source/__tests__/test.e2e.js
+++ source/__tests__/test.e2e.js
@@ -3,5 +3,6 @@ const { browser, expect, $ } = require("@wdio/globals");
 describe("Electron Testing", () => {
   it("should print application title", async () => {
     await expect($("h1")).toHaveText("Task List");
+    await browser.pause(2000);
   });
 });
