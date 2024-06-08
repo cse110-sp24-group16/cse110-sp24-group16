760dac1de28d296304b384754f139adb73273fa7
Daniel Katulevskiy <katulevskiy@gmail.com>
2024-06-08 01:14:47 -0700

    ci: Adds semantic-release

--- binaries/command.txt
+++
@@ -1,8 +0,0 @@
-zip win32-x64.zip quantum-journal-win32-x64/
-zip mas-arm64.zip quantum-journal-mas-arm64/
-zip mas-x64.zip quantum-journal-mas-x64/
-zip linux-arm64.zip quantum-journal-linux-arm64/
-zip linux-armv71.zip quantum-journal-linux-armv71/
-zip linux-x64.zip quantum-journal-linux-x64/
-zip darwin-arm64.zip quantum-journal-darwin-arm64/
-zip darwin-x64.zip quantum-journal-darwin-x64/
---
+++ build.sh
@@ -0,0 +1,11 @@
+mkdir builds
+mv quantum-journal* builds/
+cd builds
+zip quantum-journal-darwin-arm64.zip quantum-journal-darwin-arm64/
+zip quantum-journal-darwin-x64.zip quantum-journal-darwin-x64/
+zip quantum-journal-linux-arm64.zip quantum-journal-linux-arm64/
+zip quantum-journal-linux-armv7l.zip quantum-journal-linux-armv7l/
+zip quantum-journal-linux-x64.zip quantum-journal-linux-x64/
+zip quantum-journal-mas-arm64.zip quantum-journal-mas-arm64/
+zip quantum-journal-mas-x64.zip quantum-journal-mas-x64/
+zip quantum-journal-win32-x64.zip quantum-journal-win32-x64/
