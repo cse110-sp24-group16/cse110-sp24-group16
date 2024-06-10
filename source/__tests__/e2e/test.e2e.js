a3f46918d9f798104316d4ca868ec0ee020ab773
Sam <s9lau@ucsd.edu>
2024-06-09 23:03:33 -0700

    fix bug in e2e monthly test edit

--- source/__tests__/e2e/test.e2e.js
+++ source/__tests__/e2e/test.e2e.js
@@ -69,18 +69,4 @@ describe("Electron Testing", () => {
     await expect(todayDate - currentweekDate).toBe(0);
   });
 
-  //  it("check adding a new task with time", async() => {
-  //   (await $("#add-button")).click();
-  //   (await $("#new-task-title")).setValue("New Meeting");
-  //   (await $("#new-task-description")).setValue("New meeting with colleagues");
-  //   const dateInput = await browser.$("#new-task-date");    
-  //   await dateInput.setValue("2024");
-  //   await browser.keys(["ArrowRight", '0', '6', "ArrowRight", '0', '9']);
-  //   const timeInput = await browser.$("#new-task-time");
-  //   await timeInput.setValue("");
-  //   await browser.keys(["ArrowDown", "ArrowRight", '0', '9', "ArrowRight", '0', '0']);
-  //   await browser.pause(1000);
-  //   (await $("#add-task-confirm")).click();
-  //   await browser.pause(5000);
-  //  })
 });
